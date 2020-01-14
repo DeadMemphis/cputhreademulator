f275447f76bbf7c2ba6ed19a3c5e91754b7881c7
DEADMEMPHIS <krymsk_795@mail.ru>
2020-01-14 16:56:13 +0300

    update proj

---
+++ Console/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Core;
+
+namespace Threading_Test
+{
+    class Program
+    {
+        static List<Thread> ThreadPool = new List<Thread>();
+        static List<ThreadMP> MPPool = new List<ThreadMP>(); // <busy>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Core v2.0 .");
+            InitPool(2);
+            //for (int task = 0; task < 2; task++)
+            //{                
+            //    for (int mp = 0; mp < MPPool.Count; mp++)
+            //    {                    
+            //        if (MPPool[mp].state == CONTROLLER_STATES.WAITING)
+            //        {
+            //            Console.WriteLine("Thread " + mp + " set WAITING to mainThread");
+            //            ThreadPool[mp].WAKETHEFUCKUPSAMURAI();
+            //        }
+                    
+            //        if (MPPool[mp].state == CONTROLLER_STATES.END)
+            //        {                        
+            //            ThreadPool[mp].Abort();
+            //            Console.WriteLine("Thread " + mp + " aborted from mainThread");
+            //        }
+            //    }
+            //}
+            //ShutDown();
+            Console.ReadKey(); 
+        }
+       
+        static void ShutDown()
+        {
+            bool end = false;
+            for (int thr = 0; thr < ThreadPool.Count; thr++)
+            {
+                if (MPPool[thr].state == CONTROLLER_STATES.END)
+                {
+                    ThreadPool[thr].Abort();
+                    Console.WriteLine("Thread " + thr + " aborted from mainThread");
+                }
+                else if (!ThreadPool[thr].IsAlive)
+                {
+                    end = true;
+                }
+            }
+            if (!IsThreadsAlive()) Console.WriteLine("ShutDown.");
+        }
+
+        static bool IsThreadsAlive()
+        {
+            for (int thr = 0; thr < ThreadPool.Count; thr++)
+            {
+                if (ThreadPool[thr].IsAlive) return true;
+            }
+            return false;
+        }
+
+        static void InitPool(int count = 2)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                ThreadMP mp = new ThreadMP();
+                Thread thr = new Thread(mp.DoTask)
+                {
+                    IsBackground = true,
+                    Name = "MP" + i.ToString()
+                };
+                mp.Name = thr.Name;
+                mp.RequestEvent += OnRequest;
+                mp.ChangeEvent += OnStatusChanged;
+                mp.ExecuteEvent += OnExecuted;
+                MPPool.Add(mp);
+                ThreadPool.Add(thr);
+                thr.Start();
+                mp.FeatTask();
+            }
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Optimized);
+        }
+
+        static void OnStatusChanged(object sender, CONTROLLER_STATES state)
+        {
+            ThreadMP mp;
+            if (sender is ThreadMP)
+            {
+                mp = sender as ThreadMP;
+                Console.WriteLine(mp.Name + " on changed state: " + state.ToString());
+                if (state == CONTROLLER_STATES.READY) mp.SetTask(mp.TaskList.Dequeue());
+                if (mp.TaskList.Count == 0) mp.state = CONTROLLER_STATES.END;
+            }
+            else Console.WriteLine("wrong params");            
+        }
+
+        static void OnRequest(object sender, int TASK)
+        {
+            if (sender is ThreadMP)
+            {
+                Console.WriteLine("Get Request from " + (sender as ThreadMP).Name);
+            }
+            else Console.WriteLine("wrong params");
+        }
+
+        static void OnExecuted(object sender, TASK task)
+        {
+            if (sender is ThreadMP)
+            {
+                Console.WriteLine("Executed task: " + task.ToString() + " on " + (sender as ThreadMP).Name);
+            }
+            else Console.WriteLine("wrong params");
+        }
+
+    }
+
+
+    public static class Extentions
+    {
+        public static void WAKETHEFUCKUPSAMURAI(this Thread thrd)
+        {
+            thrd.Interrupt();
+            Console.WriteLine("Thread " + thrd.Name + " interrupted.");
+        }
+    }
+}
